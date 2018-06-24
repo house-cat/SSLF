24db3b9828646cb32c7887ffe81719d01a8b91ae
house-cat <stou0623@gmail.com>
2018-06-25 02:45:56 +0900

    Initial commit

---
+++ src/com/housecat/sslf/Main.java
@@ -0,0 +1,49 @@
+package com.housecat.sslf
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+
+class SSLF {
+
+    private static String program = "";
+
+    public static void loadProgramFile(String file) {
+        try {
+            File programFile = new File(file);
+            FileReader fileReader = new FileReader(programFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String programLine;
+            while ((programLine = reader.readLine()) != null) {
+                program += programLine;
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void execute() {
+        Lexer SSLFlexer= new Lexer(program);
+        ArrayList<Token> list = SSLFlexer.lex();
+
+        Parser SSLFparser = new Parser(list);
+        Interpreter SSLFinterpreter = new Interpreter(parser.getStatements());
+
+        SSLFparser.parse();
+        SSLFinterpreter.interpret();
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        SSLF.loadProgramFile(args[0]);
+        SSLF.execute();
+    }
+
+}
---
+++ src/com/housecat/sslf/Token.java
@@ -0,0 +1,111 @@
+package com.housecat.sslf;
+
+public abstract class Token<T> {
+
+    protected T value;
+
+    public T value() {
+        return value;
+    }
+}
+
+class BooleanToken extends Token<Boolean> {
+
+    public BooleanToken(String symbol) {
+        if (symbol.equals("true")) {
+            this.value = true;
+        } else {
+            this.value = false;
+        }
+    }
+}
+
+class FunctionToken extends Token<String> {
+
+    public FunctionToken(String symbol) {
+        this.value = symbol;
+    }
+}
+
+class KeyWordToken extends Token<String> {
+
+    public KeyWordToken(String symbol) {
+        this.value = symbol;
+    }
+}
+
+class NameToken extends Token<String> {
+
+    public NameToken(String symbol) {
+        this.value = symbol;
+    }
+}
+
+class NumberToken extends Token<Double> {
+
+    public NumberToken(String symbol) {
+        this.value = Double.parseDouble(symbol);
+    }
+}
+
+class OperatorToken extends Token<Operators> {
+
+    public OperatorToken(String symbol) {
+        this.value = this.getOperatorByString(symbol);
+    }
+
+    private Operators getOperatorByString(String symbol) {
+        if (symbol.equals("+")) {
+            return Operators.PLU;
+        } else if (symbol.equals("-")) {
+            return Operators.MIN;
+        } else if (symbol.equals("*")) {
+            return Operators.MUL;
+        } else if (symbol.equals(";")) {
+            return Operators.SCL;
+        } else if (symbol.equals("=")) {
+            return Operators.EQ;
+        } else if (symbol.equals("<")) {
+            return Operators.LT;
+        } else if (symbol.equals(">")) {
+            return Operators.GT;
+        } else if (symbol.equals(">=")) {
+            return Operators.GTE;
+        } else if (symbol.equals("<=")) {
+            return Operators.LTE;
+        } else if (symbol.equals("/")) {
+            return Operators.DIV;
+        } else if (symbol.equals(":")) {
+            return Operators.CLN;
+        } else if (symbol.equals("or")) {
+            return Operators.OR;
+        } else if (symbol.equals("and")) {
+            return Operators.AND;
+        } else if (symbol.equals("not")) {
+            return Operators.NOT;
+        } else if (symbol.equals("==")) {
+            return Operators.EQL;
+        } else if (symbol.equals("/=")) {
+            return Operators.NEQ;
+        } else if (symbol.equals("(")) {
+            return Operators.OB;
+        } else if (symbol.equals(")")) {
+            return Operators.CB;
+        } else if (symbol.equals("'")) {
+            return Operators.QT;
+        } else if (symbol.equals(",")) {
+            return Operators.CM;
+        } else if (symbol.equals("%")) {
+            return Operators.MOD;
+        } else {
+            throw new RuntimeException("Unknow operator.");
+        }
+    }
+}
+
+class StringToken extends Token<String> {
+
+    public StringToken(String symbol) {
+        this.value = symbol;
+    }
+}
